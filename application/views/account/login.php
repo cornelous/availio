e34d6eb4750ad5ca62d59013d0c5b9655b080869
Clive C. Banditi <clive@cornelo.us>
2014-12-02 16:19:58 +0200

    Update to template

--- application/views/account/login.php
+++ application/views/account/login.php
@@ -1,38 +1,61 @@
-<table id="tlogin">
-    <tr>
-        <th><img src="<?php echo url::base(); ?>media/images/login-logo.png" alt="<?php echo $site_name; ?>" /></th>
-        <th></th>
-    </tr>
-            <?php echo Form::open(); ?>
-            <tr>
-                <td>
-                    <?php echo Form::label('username', 'Username')?>
-                    <?php echo Form::input('username'); ?>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <?php echo Form::label('password', 'Password')?>
-                    <?php echo Form::password('password'); ?>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <?php echo Form::submit('submit', 'Login'); ?>
-                </td>
-            </tr>
+<!-- START WRAPPER -->
+<div id="wrapper">
 
-            <?php if ($errors) : ?>
-                <tr>
-                    <td class="errors">
-                        <?php echo "You have supplied invalid logins. Please try again." ?>
-                    </td>
-                </tr>
+    <!-- START LOGIN -->
+    <div id="login">
 
-            <?php endif ?>
+        <!-- START LOGIN-LOGO -->
+        <div id="login-logo"><img src="images/login-logo.png"></div>
+        <!-- END LOGIN-LOGO -->
 
-            <?php echo Form::close(); ?>
-</table>
+        <!-- START INNER -->
+        <div class="inner">
+
+            <!-- START LOGIN FORM -->
+            <form method="post" action="">
+
+                <table id="tlogin">
+                    <tr>
+                        <td>
+                            <input type="text" placeholder="Name:" name="username" value="" tabindex="1">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="password" placeholder="Password:" name="password" value="" tabindex="2">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <select style="display:none;" name="lang" class="select" tabindex="3"></select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="submit" id="btn" value="Login" style="width:100px;" tabindex="4">&nbsp;&nbsp;
+
+                            <a href="#">Register</a>
+
+                            &nbsp;&nbsp;<span style="color:#6d6e71;">|</span>&nbsp;&nbsp;
+
+                            <a href="#">Forgot Password ??</a>
+                        </td>
+                    </tr>
+                </table>
+
+            </form>
+            <!-- END LOGIN FORM -->
+
+        </div>
+        <!-- END INNER -->
+
+    </div>
+    <!-- END LOGIN -->
+
+    <div class="clear"></div>
+
+</div>
+<!-- END WRAPPER -->
 
 
 
