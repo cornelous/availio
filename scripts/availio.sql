ab87c103b300ea7fe7212a56ae195f6cca02131b
Clive C. Banditi <clive@cornelo.us>
2014-12-02 14:29:17 +0200

    update to DB schema

--- scripts/availio.sql
+++ scripts/availio.sql
@@ -5,22 +5,24 @@ USE availio;
 CREATE TABLE IF NOT EXISTS `properties` (
 `id` varchar(128) NOT NULL,
 `bbid` varchar(128) NOT NULL,
+`propertyname` varchar(128) NOT NULL,
 `propertytype` varchar(128) NOT NULL,
 `bedrooms` int(11) UNSIGNED NOT NULL,
 `sleeps` int(11) UNSIGNED NOT NULL,
 `province` varchar(128) NOT NULL,
 `city` varchar(128) NOT NULL,
 `suburb` varchar(128) NOT NULL,
+`url` varchar(128) NOT NULL,
 PRIMARY KEY (`id`),
 UNIQUE KEY `uniq_id` (`id`),
 UNIQUE KEY `uniq_bbid` (`bbid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-INSERT INTO `properties` (`id`, `bbid`, `propertytype`, `bedrooms`, `sleeps`, `province`, `city`, `suburb`) VALUES
-  ('1','17631', 'One Room Type', 2, 10, 'Western Cape', 'Capetown', 'Camps Bay'),
-  ('2','17632', 'Multipe Room Types', 1, 3, 'Western Cape', 'Capetown', 'Camps Bay'),
-  ('3','17633', 'Apartments', 4, 4, 'Western Cape', 'Capetown', 'Camps Bay');
+INSERT INTO `properties` (`id`, `bbid`, `propertyname`, `propertytype`, `bedrooms`, `sleeps`, `province`, `city`, `suburb`, `url`) VALUES
+  ('1','17631', 'API Test 1 - One Room Type', 'One Room Type', 2, 10, 'Western Cape', 'Capetown', 'Camps Bay', 'http://www.oneroom.com'),
+  ('2','17632', 'API Test 2 - Multi Room Types','Multipe Room Types', 1, 3, 'Western Cape', 'Capetown', 'Camps Bay', 'http://www.multiplerooms.com'),
+  ('3','17633', 'API Test 3 - Apartments','Apartments', 4, 4, 'Western Cape', 'Capetown', 'Camps Bay', 'http://www.apartments.com');
 
 
 
