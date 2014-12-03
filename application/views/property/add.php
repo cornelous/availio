edace4fed201734845551414d2d6ca75ebe813ce
Clive C. Banditi <clive@cornelo.us>
2014-12-03 16:25:52 +0200

    Navigation

--- application/views/property/add.php
+++ application/views/property/add.php
@@ -7,45 +7,25 @@
         <!-- START NAVIGATION -->
         <ul class="resp-tabs-list">
 
-            <?php echo HTML::anchor('property/add', 'Add Property');?>
-
             <div id="menuu">
                 <ul id="dropdown-menu" class="dropdown">
                     <li id="menuu-logo"><img src="<?php echo URL::base(); ?>media/images/menu-logo.png"></li>
-                    <?php echo HTML::anchor('property', 'Properties');?>
-                        <li class="">
-                            <div id="Properties"></div>
-                            <div id="menuuli">Properties</div>
-                        </li>
-                    </a>
-                    <a href="#" title="Seasons">
+                    <a href="#" title="Search">
                         <li class="">
                             <div id="Seasons"></div>
                             <div id="menuuli">Seasons</div>
                         </li>
                     </a>
-                    <a href="#" title="Bookings">
-                        <li class="active">
-                            <div id="Bookings"></div>
-                            <div id="menuuli">Bookings</div>
-                        </li>
-                    </a>
-                    <a href="#" title="Booking States">
+                    <a href="#" title="Properties">
                         <li class="">
-                            <div id="BookingStates"></div>
-                            <div id="menuuli">Booking States</div>
-                        </li>
-                    </a>
-                    <a href="#" title="Create Calendar Link">
-                        <li class="">
-                            <div id="CreateCalendarLink"></div>
-                            <div id="menuuli">Create Calendar Link</div>
+                            <div id="Properties"></div>
+                            <div id="menuuli">Properties</div>
                         </li>
                     </a>
                 </ul>
 
                 <ul id="menuu_icons">
-                    <?php echo HTML::anchor('logout', 'Logout');?>
+                    <a href="#" title="Logout">
                         <li>
                             <div id="Logout"></div>
                             <div id="menuuli">Logout</div>
