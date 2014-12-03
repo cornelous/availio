e53b96d8924227307cbe6a442b629dc47c4784ed
Clive C. Banditi <clive@cornelo.us>
2014-12-03 15:29:33 +0200

    Update to Repo

--- application/views/property/add.php
+++ application/views/property/add.php
@@ -1,80 +1,3 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Availability Calendar - Administration</title>
-
-    <link rel="stylesheet" href="ac-contents/themes/default/css/reset.css">
-    <link rel="stylesheet" href="css/admin-calendar.css">
-    <link rel="stylesheet" href="css/admin.css">
-    <link rel="stylesheet" href="css/mootools-roar.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="js/formcheck/theme/classic/formcheck.css"  />
-    <link rel="stylesheet" href="css/admin-pages.css">
-
-    <script type="text/javascript" src="ac-includes/js/mootools-core-1.3.2-full-compat-yc.js"></script>
-    <script type="text/javascript" src="ac-includes/js/mootools-more-1.3.2.1.js"></script>
-    <script type="text/javascript" src="js/formcheck/lang/en.js"></script>
-    <script type="text/javascript" src="js/mootools-formcheck.js"></script>
-    <script type="text/javascript" src="js/mootools-roar.js"></script>
-    <script type="text/javascript" src="js/mootools-cal-admin.js"></script>
-
-    <!--DATE PICKER-->
-    <script type="text/javascript" src="js/datepicker/jquery-1.9.1.js"></script>
-    <script type="text/javascript" src="js/datepicker/jquery-ui-1.10.1.min.js"></script>
-    <link rel="stylesheet" href="css/nigran.datepicker.css">
-    <link rel="stylesheet" href="css/jquery-ui-1.10.1.css" >
-
-    <script>
-        $(function() {
-            $( "#date-picker-input-1" ).datepicker({
-                inline: true,
-                dateFormat: 'yy-mm-dd',
-                showOtherMonths: true,
-                showOn: 'both',
-                buttonImage: 'css/images/cal_logo.png'
-            })
-
-            $( "#date-picker-input-2" ).datepicker({
-                inline: true,
-                dateFormat: 'yy-mm-dd',
-                showOtherMonths: true,
-                showOn: 'both',
-                buttonImage: 'css/images/cal_logo.png'
-            })
-
-                .datepicker('widget').wrap('<div class="ll-skin-nigran"/>');
-        });
-    </script>
-
-    <link type="text/css" rel="stylesheet" href="css/tabs/easy-responsive-tabs.css" />
-    <script src="js/tabs/easyResponsiveTabs.js" type="text/javascript"></script>
-    <script type="text/javascript">
-        $(document).ready(function () {
-            $('#horizontalTab').easyResponsiveTabs({
-                type: 'default', //Types: default, vertical, accordion           
-                width: 'auto', //auto or any width like 600px
-                fit: true,   // 100% fit in a container
-                closed: 'accordion', // Start closed if in accordion view
-                activate: function(event) { // Callback function if tab is switched
-                    var $tab = $(this);
-                    var $info = $('#tabInfo');
-                    var $name = $('span', $info);
-
-                    $name.text($tab.text());
-
-                    $info.show();
-                }
-            });
-
-            $('#verticalTab').easyResponsiveTabs({
-                type: 'vertical',
-                width: 'auto',
-                fit: true
-            });
-        });
-    </script>
-
-</head>
 <body id="page_bookings">
 <!-- START WRAPPER -->
 <div id="wrapper">
@@ -151,7 +74,7 @@
             <div id="contents">
 
                 <div id="title">
-                    <div class="float_l"><h1>Bookings - Add</h1></div>
+                    <div class="float_l"><h1>Properties - Add</h1></div>
                     <div class="clear"></div>
                 </div>
 
@@ -171,15 +94,19 @@
                             </td>
                         </tr>
                         <tr>
-                            <td class="side">Season Start Date</td>
-                            <td><input style="width:250px;" type="text" Placeholder="From Date" id="date-picker-input-1" class="my_date" name="from_date" ></td>
+                            <td class="side">External ID</td>
+                            <td><input style="width:250px;" type="text" Placeholder="ID from API" id="date-picker-input-1" class="my_date" name="from_date" ></td>
                         </tr>
                         <tr>
-                            <td class="side">Season End Date</td>
-                            <td><input style="width:250px;" type="text" Placeholder="To Date" id="date-picker-input-2" class="my_date" name="to_date" ></td>
+                            <td class="side">Avail ID</td>
+                            <td><input style="width:250px;" type="text" Placeholder="Availio ID" id="date-picker-input-2" class="my_date" name="to_date" ></td>
                         </tr>
                         <tr>
-                            <td class="side">Price</td>
+                            <td class="side">Property Name</td>
+                            <td><input type="text" name="price" placeholder="Enter the price only. Ex: 1200" style="width:250px;"></td>
+                        </tr>
+                        <tr>
+                            <td class="side">Property Type</td>
                             <td><input type="text" name="price" placeholder="Enter the price only. Ex: 1200" style="width:250px;"></td>
                         </tr>
                     </table>
@@ -210,4 +137,3 @@
 </div>
 <!-- END WRAPPER -->
 </body>
-</html>
