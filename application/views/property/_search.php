9a56f9ee3855aeeedd5838f2e2acb7ca1814d6cc
Clive C. Banditi <clive@cornelo.us>
2014-12-04 16:25:48 +0200

    Removed admin-calendar_1.css

--- application/views/property/_search.php
+++ application/views/property/_search.php
@@ -104,7 +104,7 @@
 
 <?php
 
-print_r($curlresponse);
+
     header('Content-Type: application/json');
     $est = array();
     foreach($curlresponse as $myObj){
@@ -113,28 +113,30 @@ print_r($curlresponse);
 
     $numOfest= count($est);
 
-    for ($i =0; $i <= $numOfest; $i++){
-        foreach ($est as $key => $hotel){
-            if (!is_null($hotel[$i]['bbid'])){
-                echo "{$hotel[$i]['bbid']} - {$hotel[$i]['name']}\n";
-            }
-            $rooms = array();
-            $rooms[] = $hotel[$i]['roomtypes'];
-        }
-        //print_r($rooms);
-        $numOfrooms = count($rooms[0]);
-        //echo "{$numOfrooms}\n";
+    print_r($est);
 
-        //print_r($rooms);
-        for ($idx =0; $idx <= $numOfrooms; $idx++){
-            foreach($rooms as $room){
-                echo "{$room[$idx]['roomtypename']}\n";
-                echo "{$room[$idx]['mealplans'][0]['rates']['pax1']} \n";
-                echo "{$room[$idx]['mealplans'][0]['rates']['pax2']}";
-                //for each room show daily rates
-            }
-        }
-    }
+//    for ($i =0; $i <= $numOfest; $i++){
+//        foreach ($est as $key => $hotel){
+//            if (!is_null($hotel[$i]['bbid'])){
+//                echo "{$hotel[$i]['bbid']} - {$hotel[$i]['name']}\n";
+//            }
+//            $rooms = array();
+//            $rooms[] = $hotel[$i]['roomtypes'];
+//        }
+//        //print_r($rooms);
+//        $numOfrooms = count($rooms[0]);
+//        //echo "{$numOfrooms}\n";
+//
+//        //print_r($rooms);
+//        for ($idx =0; $idx <= $numOfrooms; $idx++){
+//            foreach($rooms as $room){
+//                echo "{$room[$idx]['roomtypename']}\n";
+//                echo "{$room[$idx]['mealplans'][0]['rates']['pax1']} \n";
+//                echo "{$room[$idx]['mealplans'][0]['rates']['pax2']}";
+//                //for each room show daily rates
+//            }
+//        }
+//    }
 ?>
 
 <div id="the_months">
