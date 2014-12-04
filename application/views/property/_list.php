b2a8a7df9daabcd50adc084cc12d5036b1ab2751
Clive C. Banditi <clive@cornelo.us>
2014-12-04 03:36:40 +0200

    Added new viewlets

--- application/views/property/_list.php
+++ application/views/property/_list.php
@@ -67,7 +67,7 @@
         </div>
 
         <div class="pagination">
-            <?php var_dump($pager_links); ?>
+            <?php echo $pager_links; ?>
         </div>
 
         <!-- END ADD FORM -->
